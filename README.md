68de9f2bfb3f414be1a7cbec8e48dff7f61e4794
Tobias Isenberg <tobias.isenberg@inria.fr>
2024-06-14 16:43:37 +0200

    First version of the script, still not finished.

--- README.md
+++ README.md
@@ -27,11 +27,11 @@ Contributors to the code are:
 
 ## Code Purposes
 
-There are two types of code in this reposotory, one for reproduing one of the figures we show in the paper mentioned above (for the purpose of showing reproducibility via the [Graphics Replicability Stamp Initiative](https://www.replicabilitystamp.org/)), and the other code for supporting the continued update of the dataset.
+There are two types of code in this repository, one for reproduing one of the figures we show in the paper mentioned above (for the purpose of showing reproducibility via the [Graphics Replicability Stamp Initiative](https://www.replicabilitystamp.org/)), and the other code for supporting the continued update of the dataset.
 
 ## Paper reproducibility
 
-The code in the [`reproducibility/`](reproducibility/) subdirectory facilitates the reproduction of the plot in Figure 1 of the [corresponding paper](https://doi.org/10.1109/TVCG.2016.2615308), albeit adjusted to the updated data in the dataset (as of writing this text, years 2016--2023 of the IEEE VIS conference have been added). The original figure looks as follows:
+The code in the [`reproducibility/`](reproducibility/) subdirectory facilitates the reproduction of the plot in Figure 1 of the [corresponding paper](https://doi.org/10.1109/TVCG.2016.2615308), albeit adjusted to the updated data in the dataset (as of writing this text, years 2016–2023 of the IEEE VIS conference have been added). The original figure looks as follows:
 
 ![Figure 1 of VisPubData publication](reproducibility/figure1-original.png "Figure 1 of VisPubData publication (image is in the public domain)")
 (image is in the public domain)
@@ -41,10 +41,17 @@ The code in the [`reproducibility/`](reproducibility/) subdirectory facilitates 
 * dedicated Python libraries installed with `pip3` or `conda` as follows (or similar):
     * `altair`: `pip3 install altair` or `conda install -c conda-forge altair` (see https://altair-viz.github.io/)
     * `vl-convert`: `pip3 install vl-convert-python` or `conda install -c conda-forge vl-convert-python` (see https://altair-viz.github.io/user_guide/saving_charts.html)
-    * `beautifulsoup4`: `pip3 install beautifulsoup4` or `conda install -c conda-forge beautifulsoup4` (see https://www.crummy.com/software/BeautifulSoup/ and https://anaconda.org/anaconda/beautifulsoup4; seems to be already included with recent versions of Anaconda)
     * `pandas`: `pip3 install pandas` (see https://pandas.pydata.org/docs/getting_started/install.html; already included in Anaconda)
 
 ### Running the script
+
+The [`reproducibility/reproducibility.py`](reproducibility/reproducibility.py) essentially loads the current state of the dataset  and then directly produces the new version of the figure in the local directory. Because it is not possible to download the corresponding data from the script without authentication, please first go to the shared [Google Spreadsheet](https://docs.google.com/spreadsheets/d/1xgoOPu28dQSSGPIp_HHQs0uvvcyLNdkMF9XtRajhhxU/edit?usp=sharing) that contains the data, make sure that the first tab on the bottom is selectted ("Main dataset"), and then download the data using the menu vis File > Download > Comma Separated Values (.csv) and then save the file to [`reproducibility/vispubdata.csv`](reproducibility/vispubdata.csv).
+
+To run the script, simply do:
+```
+cd reproducibility/
+python3 reproducibility.py
+```
 
 work in progress ...
 
---
+++ reproducibility/reproducibility.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python3
+
+from urllib.request import urlopen
+import os,sys
+import pandas as pd
+import altair as alt
+import csv
+from math import nan
+import math
+
+#####################################
+# overall settings
+#####################################
+inputfilename = "vispubdata.csv"
+outputfilename = "reproducibility.pdf"
+googleSheetsLocation = "https://docs.google.com/spreadsheets/d/1xgoOPu28dQSSGPIp_HHQs0uvvcyLNdkMF9XtRajhhxU/edit?usp=sharing"
+visPadding = 5
+
+#####################################
+# change to directory of the script
+#####################################
+pathOfTheScript = os.path.dirname(sys.argv[0])
+if pathOfTheScript != "": os.chdir(pathOfTheScript)
+
+#####################################
+# main parts of the script
+#####################################
+
+# check if the needed data file exists
+if (os.path.isfile(inputfilename)):
+    vispubdataList = []
+    # load the previously downloaded data file
+    with open(inputfilename, 'r', encoding="utf-8") as csvfile:
+        # create a CSV reader object
+        reader = csv.DictReader(csvfile)
+        # iterate over the rows
+        for row in reader:
+            dataItem = {}
+            dataItem['doi'] = row['DOI'].lower()
+            dataItem['year'] = int(row['Year'])
+            dataItem['papertype'] = row['PaperType']
+            dataItem['venue'] = row['Conference']
+
+            if dataItem['papertype'] in ['C', 'J']:
+                vispubdataList.append(dataItem)
+    
+    # find first and last year of the dataset, and all venues
+    listOfVenuesToShow = ["VIS/VisWeek incl. TVCG/CG&A", "VIS/VisWeek w/o TVCG/CG&A", "Vis (1990-2011)", "SciVis (2012 onward)", "VAST", "InfoVis", "SciVis conf.", "VAST conf.", "TVCG @ VIS", "CG&A @ VIS"]
+    firstYear = vispubdataList[0]['year']
+    lastYear = vispubdataList[0]['year']
+    listOfVenuesInData = []
+    for item in vispubdataList:
+        if item['year'] < firstYear: firstYear = item['year']
+        if item['year'] > lastYear: lastYear = item['year']
+        if not item['venue'] in listOfVenuesInData: listOfVenuesInData.append(item['venue'])
+    # print(listOfVenuesInData) # ['Vis', 'VAST', 'InfoVis', 'SciVis']
+
+    # create the empty dataset, still with the original venue names
+    visPubDataAggregation = {}
+    for venue in listOfVenuesInData:
+        visPubDataAggregation[venue + "-C"] = {}
+        visPubDataAggregation[venue + "-J"] = {}
+        for year in range(firstYear, lastYear + 1):
+            visPubDataAggregation[venue + "-C"][year] = nan
+            visPubDataAggregation[venue + "-J"][year] = nan
+
+    # fill the aggregated data with entries from the list, still with the original venue names
+    for item in vispubdataList:
+        year = item['year']
+        venue = item['venue']
+        type = item['papertype']
+        if math.isnan(visPubDataAggregation[venue + "-" + type][year]): visPubDataAggregation[venue + "-" + type][year] = 1
+        else: visPubDataAggregation[venue + "-" + type][year] += 1
+
+    # now let's transfer the data to the more complex scheme, starting with the empty set
+    visPubDataAggregationDetailed = {}
+    for venue in listOfVenuesToShow:
+        visPubDataAggregationDetailed[venue] = {}
+        for year in range(firstYear, lastYear + 1):
+            visPubDataAggregationDetailed[venue][year] = nan
+    
+    # this needs some background info on naming and years
+    # first, the original Vis conference, from 1990 to 2011 (inclusive); up to 2005 they were conference papers
+    for year in range(firstYear, 2006): visPubDataAggregationDetailed[listOfVenuesToShow[2]][year] = visPubDataAggregation['Vis-C'][year]
+    for year in range(2006, 2012): visPubDataAggregationDetailed[listOfVenuesToShow[2]][year] = visPubDataAggregation['Vis-J'][year]
+    # then the (renamed) SciVis conference, technically from 2012 but since we want to connect the lines we start from 2011
+    visPubDataAggregationDetailed[listOfVenuesToShow[3]][2011] = visPubDataAggregation['Vis-J'][2011]
+    for year in range(2012, lastYear + 1): visPubDataAggregationDetailed[listOfVenuesToShow[3]][year] = visPubDataAggregation['SciVis-J'][year]
+    # InfoVis is easy; up to 2005 as conference, then as journal
+    for year in range(firstYear, 2006): visPubDataAggregationDetailed[listOfVenuesToShow[5]][year] = visPubDataAggregation['InfoVis-C'][year]
+    for year in range(2006, lastYear + 1): visPubDataAggregationDetailed[listOfVenuesToShow[5]][year] = visPubDataAggregation['InfoVis-J'][year]
+    # so is VAST; up to 2012 as conference, then as journal
+    for year in range(firstYear, 2013): visPubDataAggregationDetailed[listOfVenuesToShow[4]][year] = visPubDataAggregation['VAST-C'][year]
+    for year in range(2013, lastYear + 1): visPubDataAggregationDetailed[listOfVenuesToShow[4]][year] = visPubDataAggregation['VAST-J'][year]
+    # VAST conference from 2014 on
+    for year in range(2014, lastYear + 1): visPubDataAggregationDetailed[listOfVenuesToShow[7]][year] = visPubDataAggregation['VAST-C'][year]
+
+    # print(visPubDataAggregationDetailed)
+
+    # finally, sum up the totals
+    for year in range(firstYear, lastYear + 1):
+        sum = 0
+        for venue in listOfVenuesToShow[2:6]: # we do not include the later conference-only papers
+            if not math.isnan(visPubDataAggregationDetailed[venue][year]):
+                sum += visPubDataAggregationDetailed[venue][year]
+        visPubDataAggregationDetailed[listOfVenuesToShow[1]][year] = sum
+        sum = 0
+        for venue in listOfVenuesToShow[2:6]: # we do not include the later conference-only papers
+            if not math.isnan(visPubDataAggregationDetailed[venue][year]):
+                sum += visPubDataAggregationDetailed[venue][year]
+        for venue in listOfVenuesToShow[9:10]: # but here the journal presentations
+            if not math.isnan(visPubDataAggregationDetailed[venue][year]):
+                sum += visPubDataAggregationDetailed[venue][year]
+        visPubDataAggregationDetailed[listOfVenuesToShow[0]][year] = sum
+    # but without counting Vis/SciVis 2011 twice (see hack from before):
+    visPubDataAggregationDetailed[listOfVenuesToShow[1]][2011] -= visPubDataAggregationDetailed[listOfVenuesToShow[3]][2011]
+    visPubDataAggregationDetailed[listOfVenuesToShow[0]][2011] -= visPubDataAggregationDetailed[listOfVenuesToShow[3]][2011]
+
+    # and add the final unified VIS starting from 2021
+    for year in range(2021, lastYear + 1):
+        visPubDataAggregationDetailed[listOfVenuesToShow[1]][year] += visPubDataAggregation['Vis-J'][year]
+        visPubDataAggregationDetailed[listOfVenuesToShow[0]][year] += visPubDataAggregation['Vis-J'][year]
+
+    # empty years
+    for year in range(firstYear, lastYear + 1):
+        if visPubDataAggregationDetailed[listOfVenuesToShow[1]][year] == 0: visPubDataAggregationDetailed[listOfVenuesToShow[1]][year] = nan
+        if visPubDataAggregationDetailed[listOfVenuesToShow[0]][year] == 0: visPubDataAggregationDetailed[listOfVenuesToShow[0]][year] = nan
+
+    # now for the visualization
+    dataToPlot = []
+    for venue in listOfVenuesToShow: # redo the data for the line graph, because this is not stacked
+        for year in range(firstYear, lastYear + 1):
+            dataToPlot.append({"venue": venue, "name": venue, "year": year, "count": visPubDataAggregationDetailed[venue][year]})#, "order": color_number * 2, "order2": color_number})
+    altairData = pd.DataFrame(dataToPlot)
+
+    chart = alt.Chart(altairData, width={"step": 15}).mark_line().encode(
+        x = alt.X('year:N', title=None).axis(labelAngle=-45),#domain=False, 
+        y = alt.Y('count:Q', title=None),
+        color = alt.Color('name:N', sort=None, title=None),
+        order = alt.Order("venue:Q")
+    ).configure_range(
+        #                           l gray     d gray     orange    blue       green       red        l blue     l green   l purple   d purple
+        category=alt.RangeScheme(['#aaaaaa', '#444444', '#f58518', '#4c78a8', '#54a24b', '#e45756', '#77a2d4', '#84d278', '#b279a2', '#5f2d53', '#ff9da6', '#bab0ac'])
+    ).configure_legend(columns=4, orient='bottom', direction='horizontal', titleLimit=0, labelLimit=0
+    ).configure_view(strokeWidth=0).properties(
+        padding={"left": visPadding, "right": visPadding, "bottom": visPadding, "top": visPadding},
+        width=600,
+        height=300
+    )
+    chart.save(outputfilename)
+
+else:
+    print("The input data file " + inputfilename + " was not found, please first go the shared Google Sheets location.")
+    print("There download the VisPubData dataset as a CSV file to " + inputfilename + ". The url for the data is:")
+    print(googleSheetsLocation)
